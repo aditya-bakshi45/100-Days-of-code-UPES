f843246125f6e71a91edfc85c58988547984f762
Aditya Sharma <vsadityasharma2909@gmail.com>
2026-09-18 21:33:51 +0530

    Add files via upload

---
+++ sumdiagonal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d %d", &n, &n);
+
+    int a[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i][i];
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
